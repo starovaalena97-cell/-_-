2708fd7cd72c857dd1440973c0d3ee2d57febade
Alena Starova <starovaalena@mail.ru>
2026-06-05 11:52:39 +0300

    обновление: добавил модуль widget и обновил тесты

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -2,7 +2,7 @@
 Тесты для модуля masks.
 """
 
-from src.masks import get_mask_card_number, get_mask_account
+from src.masks import get_mask_account, get_mask_card_number
 
 
 def test_get_mask_card_number():
